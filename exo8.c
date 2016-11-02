6a98180bb0530da18d7a5d25fa2ce0fc8bddca54
Utilisateur <thibaudlarcher@gmail.com>
2016-11-02 21:25:28 +0100

    test5

--- exo8.c
+++ exo8.c
@@ -6,13 +6,9 @@
 	 i=2;
 	scanf("%d",&n);
 	while(i!=(n-1)){
-		
 		if(n%i==0){printf("faux\n"); return 0;}
-		else {printf("vrais\n"); return 0;}
 		i++;
-		
-		
 	}
-	 
+	 printf("vrais\n");
 	 return 0;
  }
